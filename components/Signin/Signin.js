174ef5095ccd7850899f2f5fb29ae22570a4d56f
Mehrad Rousta <mehrad77@gmail.com>
2018-12-19 20:11:44 +0330

    add sigin component basic

---
+++ components/Signin/Signin.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
+import Error from './ErrorMessage';
+import { CURRENT_USER_QUERY } from '../Queries/User';
+
+const SIGNIN_MUTATION = gql`
+
+`;
+
+class Signin extends Component {
+
+  render() {
+    return (
+      <>
+      </>
+    );
+    
+  }
+}
+
+export default Signin;
