9ec72ddff1cecbffd20766ef7f43f9a8ac3fc340
Mehrad Rousta <mehrad77@gmail.com>
2018-12-22 09:16:38 +0330

    add basics of  "AddPodcastForm" component and page

---
+++ components/AddPodcastForm/AddPodcastForm.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import { Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
+import { Flex } from '@rebass/grid';
+import styled from 'styled-components';
+import Form from '../styles/Form';
+import Error from '../ErrorMessage';
+import { CURRENT_USER_QUERY } from '../Queries/User';
+
+const ADD_PODCAST_FROM_URL_MUTATION = gql`
+  mutation ADD_PODCAST_FROM_URL_MUTATION($rss: String!) {
+    addPodcastFromURL(rss: $rss) {
+      id
+      rss
+    }
+  }
+`;
+
+class AddPodcastForm extends Component {
+  state = {
+    rss: '',
+  };
+  saveToState = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  render() {
+    return (
+      <Mutation
+        mutation={ADD_PODCAST_FROM_URL_MUTATION}
+        variables={this.state}
+      >
+        {(addpodcast, { error, loading }) => (
+          <Form
+            method="post"
+            onSubmit={async e => {
+              e.preventDefault();
+              await addpodcast();
+              this.setState({ rss: '' });
+            }}
+          >
+            <fieldset disabled={loading} aria-busy={loading}>
+              <h2>Add A Podcast To Bungin Database</h2>
+              <Error error={error} />
+              
+              <label htmlFor="name">
+                Name
+                <input
+                  type="text"
+                  name="rss"
+                  placeholder="Put RSS URL here..."
+                  value={this.state.name}
+                  onChange={this.saveToState}
+                />
+              </label>
+             
+
+              <button type="submit">Add Podcast!</button>
+            </fieldset>
+          </Form>
+        )}
+      </Mutation>
+    );
+  }
+}
+
+export default AddPodcastForm;
--- pages/add-podcast.js
+++ pages/add-podcast.js
@@ -1,9 +1,41 @@
-import Link from 'next/link';
+import * as React from 'react';
+import { Flex } from '@rebass/grid';
+import styled from 'styled-components';
+import { SloganTitle } from '../components/elements/Typography';
+import { Container } from '../components/elements/Layout';
+import AddPodcastForm from '../components/AddPodcastForm';
 
-const AddPodcast = props => (
-  <div>
-    <p>AddPodcast!</p>
-  </div>
+const Columns = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-gap: 20px;
+`;
+
+const Right = styled.div`
+  grid-area: menu;
+`;
+
+const Left = styled.div`
+
+`;
+
+
+const Add_Podcast = (props) => (
+    <Flex flex="1 1 auto" flexDirection="column">
+        <Flex py={5} flexDirection="column" alignItems="center" as="section">
+            <Container justifyContent="flex-start" flexDirection="column">
+                <SloganTitle>Help Us Grow Our Diversity.</SloganTitle>
+                <Columns>
+                    <Right>
+                        <AddPodcastForm/>
+                    </Right>
+                    <Left>
+                        
+                    </Left>
+                </Columns>
+            </Container>
+        </Flex>
+    </Flex>
 );
 
-export default AddPodcast;
+export default Add_Podcast;
