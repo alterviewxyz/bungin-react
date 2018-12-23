e9bcadb12af6b3e151a4f46b749a17f9f0bdb5b0
Mehrad Rousta <mehrad77@gmail.com>
2018-12-23 15:05:58 +0330

    add cleanURL to Next

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
 import Link from 'next/link';
 
 const Home = props => (
-    <p>Hey!</p>
+    <>
+        <Link as={`/p/cjpznji6caqjl0a988eiptvhr`} href={`/p?id=cjpznji6caqjl0a988eiptvhr`}>
+            <p>A Sample Podcast!</p>
+        </Link>
+    </>
 );
 
 export default Home;
