30a39e029b3becc37dc0626cd3ac51a00fd32047
Mehrad Rousta <mehrad77@gmail.com>
2018-12-25 04:09:50 +0330

    add PublishEpisodesForm

---
+++ components/PublishLevels/PublishEpisodesForm/PublishEpisodesForm.js
@@ -0,0 +1,119 @@
+import React, { Component } from 'react';
+import { Mutation, Query } from 'react-apollo';
+import gql from 'graphql-tag';
+import Router from 'next/router';
+import { Flex } from '@rebass/grid';
+import styled from 'styled-components';
+import Form from '../../styles/Form';
+import Error from '../../ErrorMessage';
+import { CURRENT_USER_QUERY } from '../../Queries/User';
+
+const SINGLE_PODCAST_STATION_QUERY = gql`
+  query SINGLE_PODCAST_STATION_QUERY($id: ID!) {
+    podcastStation(where: { id: $id }) {
+      id
+      slug
+      rss
+      title
+      description
+      image
+      language
+      website
+      unProcessedFeed
+      episodesId {
+        id
+      }
+    }
+  }
+`;
+
+const CREATE_PODCAST_EPISODE = gql`
+  mutation CREATE_PODCAST_EPISODE(
+    $mp3: String!,
+    $title: String!,
+    $description: String!,
+    $image: String,
+    $episodeNubmer: Int!)  {
+      createPodcastEpisode(
+        data: {
+          mp3: $mp3, title: $title, description: $description, image: $image, episodeNubmer: $episodeNubmer
+        }
+      )
+      {
+        id
+        title
+        description
+        episodeNubmer
+      }
+  }
+`;
+
+class PublishEpisodesForm extends Component {
+  state = {
+  };
+  
+  saveToState = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  createPodcastEpisode = async (e, createPodcastEpisodeMutation) => {
+    e.preventDefault();
+    if((this.state.nextToGo + 1) > this.state.length){
+      Router.push({
+        pathname: '/publish/step3',
+        query: {id:this.props.id}
+      });
+      return true;
+    }
+    console.log('Create Podcast Episode!!');
+    const res = await createPodcastEpisodeMutation({
+      variables: {
+          mp3:          this.state.Episodes[this.state.nextToGo].title,
+          episodeNubmer:this.state.nextToGo,
+          title:        this.state.Episodes[this.state.nextToGo].title,
+          description:  this.state.Episodes[this.state.nextToGo].content,
+          image:        this.state.Episodes[this.state.nextToGo].image,
+      },
+    });
+    console.log('Created!!',res);
+    this.setState({ nextToGo: this.state.nextToGo + 1});
+    console.log('next:  ',this.state.nextToGo);
+  };
+
+  render() {
+    return (
+      <Query
+        query={SINGLE_PODCAST_STATION_QUERY}
+        variables={{
+          id: this.props.id
+        }}>
+        {({ error, loading, data }) => {
+          if (error) return <Error error={error} />;
+          if (loading) return <p>Loading...</p>;
+          if (!data.podcastStation)
+            return <p>No Podcast Found for {this.props.id}</p>;
+            const Episodes = data.podcastStation.unProcessedFeed.Episodes;
+
+            //ُThis isn't the right what but I couldn't find any better way still
+            this.setState((prevState) => {
+              if (prevState.length === Episodes.length) return null
+              return { length: Episodes.length, Episodes, nextToGo: 0 }
+            });
+            console.log(Episodes[this.state.nextToGo]);
+            
+          return (
+            <Mutation mutation={CREATE_PODCAST_EPISODE} variables={this.state}>
+            {(createPodcastEpisode, { loading, error }) => (
+              <Form onSubmit={e => this.createPodcastEpisode(e, createPodcastEpisode)}>
+                <button type="submit">Episode {this.state.nextToGo} of {this.state.length}!</button>
+              </Form>
+            )}
+          </Mutation>
+
+        )}}
+      </Query>
+    );
+  }
+}
+
+export default PublishEpisodesForm;
---
+++ pages/publish/step2.js
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import { Flex } from '@rebass/grid';
+import styled from 'styled-components';
+import { SloganTitle } from '../../components/elements/Typography';
+import { Container } from '../../components/elements/Layout';
+import PublishEpisodesForm from '../../components/PublishLevels/PublishEpisodesForm';
+import PleaseSignIn from '../../components/PleaseSignIn';
+
+const Columns = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-gap: 20px;
+`;
+
+
+const Add_Podcast = (props) => (
+    <Flex flex="1 1 auto" flexDirection="column">
+        <Flex py={5} flexDirection="column" alignItems="center" as="section">
+            <Container justifyContent="flex-start" flexDirection="column">
+                <PleaseSignIn>
+                    <SloganTitle>Help Us Grow Our Diversity.</SloganTitle>
+                    <Columns>
+                        <PublishEpisodesForm id={props.query.id} />
+                    </Columns>
+                </PleaseSignIn>
+            </Container>
+        </Flex>
+    </Flex>
+);
+
+export default Add_Podcast;
