fc48d63e3f8f1324b926dd2107b8488840031601
Mehrad Rousta <mehrad77@gmail.com>
2018-12-29 07:06:26 +0330

    list podcasts in home page

---
+++ components/ListPodcasts/ListPodcasts.js
@@ -0,0 +1,34 @@
+import { Query } from 'react-apollo';
+import React from 'react';
+import Link from 'next/link';
+import styled from 'styled-components';
+import PodcastCard from '../PublishLevels/ManagePodcasts/PodcastCard';
+import { ALL_PODCAST_STATIONS_QUERY } from '../Queries/Queries';
+
+const ItemsList = styled.div`
+  display: grid;
+  grid-template-columns: auto;
+  grid-gap: 10px;
+  max-width: ${props => props.theme.maxWidth};
+  margin: 0 auto;
+`;
+
+const ManagePodcasts = props => (
+  <Query query={ALL_PODCAST_STATIONS_QUERY}>
+    {({ data, error, loading }) => {
+      if (loading) return <p>Loading...</p>;
+      if (error) return <p>Error: {error.message}</p>;
+      return (
+        <ItemsList>
+          {data.podcastStations.map(item => (
+            <Link as={`/p/${item.slug}`} href={`/p?slug=${item.slug}`}>
+              {item.title}
+            </Link>
+          ))}
+        </ItemsList>
+      );
+    }}
+  </Query>
+);
+
+export default ManagePodcasts;
--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,7 @@ import ListPodcasts from '../components/ListPodcasts';
 
 const Home = props => (
   <>
-    {/* <ListPodcasts /> */}
-    <Link as="/p/radio-marz" href="/p?slug=radio-marz">
-      <p>A Sample Podcast!</p>
-    </Link>
+    <ListPodcasts />
   </>
 );
 
